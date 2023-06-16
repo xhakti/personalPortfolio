374849e28cf26d63cc351f66cfd886c03ddb257b
Shakti Dubey <shaktidubey@shaktidubey.local>
2023-06-16 18:06:38 +0530

    addingProject_BMB

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,32 +1,33 @@
-import Project1 from '../../assets/work1.jpg'
+import Project1 from '../../assets/BuyMeABeer.png'
 import Project2 from '../../assets/work2.jpg'
 import Project3  from '../../assets/work3.jpg'
 
 
 export const projectsData = [
-    {
-        id: 1, 
-        image: Project1, 
-        title: 'NFT MarketPlace',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus cupiditate earum iusto ipsum quod eveniet expedita sed accusantium commodi minus aliquid neque totam, voluptatibus dolore ducimus qui. Deserunt, cum odit!',   
-        tags: ["React", "Solidity", "Hardhat", "IPFS", "Remix", "Web3js"]
-    },
+  {
+    id: 1,
+    image: Project1,
+    title: "Buy Me A Beer",
+    description:
+      'Buy Me a Beer is a satirical decentralized application (dApp) that parodies the popular crowdfunding platform "Buy Me a Coffee." Developed with Next.js, Solidity, and Thirdweb technologies, this dApp offers users the ability to support content creators or friends by virtually buying beers instead of coffees. ',
+    tags: ["Solidity", "NextJS", "Chakra UI", "Thirdweb", "Hardhat", "Ethers"],
+  },
 
-    {
-        id: 2, 
-        image: Project2, 
-        title: 'Defi Lending Protocol',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus cupiditate earum iusto ipsum quod eveniet expedita sed accusantium commodi minus aliquid neque totam, voluptatibus dolore ducimus qui. Deserunt, cum odit!',   
-        tags: ["React", "Solidity", "Hardhat", "Alchemy", "Remix", "Web3js"]
-    },
-    {
-        id: 3, 
-        image: Project3, 
-        title: 'Nft Staking Game',
-        description: 'Nft staking game',   
-        tags: ["React", "Solidity",  "Unity", "Remix", "Web3js"]
-    }    
-
-]
+  {
+    id: 2,
+    image: Project2,
+    title: "Defi Lending Protocol",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus cupiditate earum iusto ipsum quod eveniet expedita sed accusantium commodi minus aliquid neque totam, voluptatibus dolore ducimus qui. Deserunt, cum odit!",
+    tags: ["React", "Solidity", "Hardhat", "Alchemy", "Remix", "Web3js"],
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Nft Staking Game",
+    description: "Nft staking game",
+    tags: ["React", "Solidity", "Unity", "Remix", "Web3js"],
+  },
+];
 
 export default projectsData;
