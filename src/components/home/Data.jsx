75d3da786a2eb88c6abcbdc43ba04a94797b83bb
xhakti <dubeyshakti0827@gmail.com>
2024-05-09 19:56:43 +0530

    testing contri his

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -2,7 +2,7 @@ const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
-        Shakti 🔥🦉
+        Shakti 🔥🦉🌎
         {/* The svg for the hand emoji */}
         
       </h1>
